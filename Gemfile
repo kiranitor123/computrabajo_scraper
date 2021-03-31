64265594983b56a452ad4f3dc46d13a5e519d1c6
Rolando Alvarez <rolandoalvarezfaye@gmail.com>
2021-03-31 16:11:20 -0400

    first attemp for get working the scraper

--- Gemfile
+++ Gemfile
@@ -1,10 +1,10 @@
 source 'https://rubygems.org'
 
-gem 'rubocop', '~>0.81.0', require: false
-gem 'rspec', '~> 3.4'
-gem 'solargraph'
 gem 'byebug'
+gem 'rspec', '~> 3.4'
+gem 'rubocop', '~>0.81.0', require: false
 gem 'httparty'
+gem 'solargraph'
 gem 'nokogiri'
 gem 'watir'
 gem 'webdrivers'
--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,32 @@
 #!/usr/bin/env ruby
-p 'Hello world'
+# frozen_string_literal: true
+
+require 'nokogiri'
+require 'httparty'
+require 'byebug'
+
+def scrapper
+  url = 'https://www.trabajopolis.bo/bolsa-de-trabajo-empleos-en-bolivia/Cochabamba/?searchId=142899.1723&action=search&page=1&listings_per_page=10'
+  unparsed_page = HTTParty.get(url)
+  parsed_page = Nokogiri::HTML(unparsed_page)
+  job_list = parsed_page.css('tr.priorityListing')
+  pages_in = job_list.count
+  page = 1
+
+  all_jobs = parsed_page.css('div.numberResults')[0].text.split(' ')[0].gsub(',', '').to_i
+  last_page = (all_jobs.to_f / pages_in.to_f).round
+  jobslists = []
+  job_list.each do |jobs|
+    job = {
+      title: jobs.css('span.anuncio-estandar-titulo').text,
+      company: jobs.css('span.anuncio-estandar-empresa').text,
+      location: jobs.css('span.anuncio-estandar-ciudad').text,
+      date: jobs.css('span.date').text,
+      ulr: jobs.css('a')[0].attributes['href'].value
+    }
+    jobslists << job
+  end
+  byebug
+end
+
+scrapper
